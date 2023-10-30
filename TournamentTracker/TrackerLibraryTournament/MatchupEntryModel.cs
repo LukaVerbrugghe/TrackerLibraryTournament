d4e8b9cf5aa2ea7396c66cecc7a889daa8a4185b
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-10-30 20:44:58 +0100

    02:06:16

--- TournamentTracker/TrackerLibraryTournament/MatchupEntryModel.cs
+++ TournamentTracker/TrackerLibraryTournament/MatchupEntryModel.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace TrackerLibraryTournament {
     public class MatchupEntryModel {
+        /// <summary>
+        /// Represents one  team in the matchup
+        /// </summary>
         public TeamModel TeamCompeting { get; set; }
+        /// <summary>
+        /// Reperesents the score of this particular team
+        /// </summary>
         public double Score { get; set; }
+        /// <summary>
+        /// Representss the matchup that this team came from as the winner
+        /// </summary>
         public MatchupModel ParentMatchup { get; set; }
     }
 }
